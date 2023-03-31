b48cee823b28d91e8a0776461d18dadaab5df3ad
LunashaGit <lunamuylkens@gmail.com>
2023-03-31 10:43:23 +0200

    Simplest Popup in summary Section

--- resources/js/Components/Custom/Summary.tsx
+++ resources/js/Components/Custom/Summary.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import GithubPicture from "./GithubPicture";
+import { Transition } from "@headlessui/react";
 type Users = {
     email: string;
     first_name: string;
@@ -9,11 +10,15 @@ type Users = {
 };
 export default function Summary(props) {
     const [users, setUsers] = useState<Users[]>([]);
+    const [isClicked, setIsClicked] = useState<boolean>(false);
+    const [clickedUser, setClickedUser] = useState<Users>();
 
     useEffect(() => {
         setUsers(props.infos);
     }, [props.infos]);
 
+    console.log(isClicked);
+    console.log(clickedUser);
     return (
         <div>
             {users && (
@@ -39,6 +44,10 @@ export default function Summary(props) {
                             <tr
                                 key={index}
                                 className="text-left hover:bg-[#202c34] cursor-pointer p-2"
+                                onClick={() => {
+                                    setIsClicked(true);
+                                    setClickedUser(user);
+                                }}
                             >
                                 <td className=" bg-[#373f50] border-[#202c34] border-2">
                                     <GithubPicture
@@ -60,6 +69,38 @@ export default function Summary(props) {
                     </tbody>
                 </table>
             )}
+            <Transition
+                show={isClicked}
+                enter="transition-opacity duration-500"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="transition-opacity duration-500"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+            >
+                <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
+                    <div className="bg-[#373f50] w-1/2 h-1/2 rounded-lg flex flex-col justify-center items-center">
+                        <GithubPicture
+                            className="h-16 w-16"
+                            user={clickedUser.github_id}
+                        />
+                        <p className="text-white text-2xl">
+                            {clickedUser.first_name} {clickedUser.last_name}
+                        </p>
+                        <p className="text-white text-2xl">
+                            {clickedUser.percentage}%
+                        </p>
+                        <button
+                            className="bg-red-500 text-white rounded-lg p-2"
+                            onClick={() => {
+                                setIsClicked(false);
+                            }}
+                        >
+                            Close
+                        </button>
+                    </div>
+                </div>
+            </Transition>
         </div>
     );
 }
